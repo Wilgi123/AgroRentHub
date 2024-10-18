d6930850bc9716803ad203ef641164f8945d7f94
Wilgimol Thomas <130832399+Wilgi123@users.noreply.github.com>
2024-10-18 18:48:25 +0530

    Update settings.py

--- AgroRentHub/settings.py
+++ AgroRentHub/settings.py
@@ -127,9 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Add this only if your static files are inside a 'static' folder within your app.
+
 '''STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]'''
 
 # Media files
