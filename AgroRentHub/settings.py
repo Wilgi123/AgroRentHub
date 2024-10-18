3aa17f4fc4e0fbb897979e8c2fa525ae922c937b
Wilgimol Thomas <130832399+Wilgi123@users.noreply.github.com>
2024-10-18 12:24:20 +0530

    Create settings.py

---
+++ AgroRentHub/settings.py
@@ -0,0 +1,176 @@
+
+import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-r=hvs657cc-!!xkiba!or2e^1-(c^vzz4#d*i&4^93uf0v^r6v'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['wilgimolthomas2.repl.co']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'agroapp',
+    'social_django',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'agroapp.middleware.DisableBrowserCacheMiddleware',
+    'agroapp.middleware.RemoveExpiredCartItemsMiddleware',
+]
+
+ROOT_URLCONF = 'AgroRentHub.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'], 
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                 'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
+            ],
+        },
+    },
+]
+CRONJOBS = [
+    ('0 0 * * *', 'customer.cron.remove_expired_cart_items')
+]
+WSGI_APPLICATION = 'AgroRentHub.wsgi.application'
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.google.GoogleOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '777614889146-upajos9idta89d28k5fo8pq8nk8c5pgg.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email', 'profile']
+LOGIN_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'logout'
+SOCIAL_AUTH_GOOGLE_OAUTH2_REDIRECT_URI = 'http://127.0.0.1:8000/social-auth/complete/google-oauth2/'
+
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+#TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+#USE_TZ = True
+TIME_ZONE = 'Asia/Kolkata'
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+'''STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]'''
+
+# Media files
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Ensure you have the following settings for session management
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Use database-backed sessions
+SESSION_COOKIE_AGE = 1209600  # Two weeks, in seconds
+SESSION_SAVE_EVERY_REQUEST = True
+#465
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = 'sachu7589@gmail.com'
+#EMAIL_HOST_PASSWORD = 'atyw tqqv heqc lpfx'
+EMAIL_HOST_USER = 'agrorenthub@gmail.com'
+EMAIL_HOST_PASSWORD = 'ecry rjfl ufdb bnfd'
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
+CELERY_BEAT_SCHEDULE = {
+    'remove-expired-cart-items': {
+        'task': 'agroapp.tasks.remove_expired_cart_items',
+        'schedule': 86400.0,  # Run daily (seconds)
+    },
+}
+
+#PAYPAL_CLIENT_ID = 'AXjxoplaMeNDaQKZgqtBvzNk9taph53iKy4ZprQuQ1PwLKrzOSIfGGO584tmi7cmGpGX1RGCHaNb5gjH'
+#PAYPAL_SECRET_KEY = 'EA2-IMwpcZHKZlHFp1CDvcnw078bXMLdiX3P66Mz9OJTGweJpxrmjzZW0sfA1SCUIwoyvjM2ORz6AHXa'
+RAZORPAY_KEY_ID = 'rzp_test_IHsYiv67CqdAaf'
+RAZORPAY_KEY_SECRET = 'xP9T994L6wieFJYHjb1wfPNO'
